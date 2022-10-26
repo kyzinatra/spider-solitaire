771cade9f8e5be99392b18541d2e0785ccb26d9b
kyzinatra <main@kyzinatra.ru>
2022-10-26 16:50:22 +0300

    * Add card * Clear public

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Card from "../Card/Card";
 
 const App = () => {
-  return <div>This is my app</div>;
+  return (
+    <div>
+      This is my app
+      <Card />
+    </div>
+  );
 };
 
 export default App;
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,13 +3,11 @@ import { ReportHandler } from "web-vitals";
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      console.log(
-        getCLS(onPerfEntry),
-        getFID(onPerfEntry),
-        getFCP(onPerfEntry),
-        getLCP(onPerfEntry),
-        getTTFB(onPerfEntry)
-      );
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
     });
   }
 };
