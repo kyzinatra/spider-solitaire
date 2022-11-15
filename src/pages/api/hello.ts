157e5bb0bc2825eb01c4763953c0487eb71269b8
kyzinatra <main@kyzinatra.ru>
2022-11-15 19:20:44 +0300

    test admin sdk

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,10 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { app } from "../../../admin.config";
 
 type Data = {
   name: string;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  console.log(app);
   res.status(200).json({ name: "John Doe" });
 }
